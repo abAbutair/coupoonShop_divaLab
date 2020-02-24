1331a7f2b911ad0e200607b9bcafcc060ddd9b7e
Abdulrahman Abutair <ab.hamdy@diva-lab.com>
2020-02-24 12:19:17 +0200

    make dropdown.js file

--- js/mainScript.js
+++ js/mainScript.js
@@ -1,11 +1,11 @@
 // Diva-Lab @2020 Main Script
 ////////////////////////////////////////
 
-
 // CSS vh(viewport height) reset
 let jvh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--jvh', `${jvh}px`);
 
+// get element siblings
 let getSiblings = function (elem) {
     // Setup siblings array and get the first sibling
     let siblings = [];
@@ -21,73 +21,4 @@ let getSiblings = function (elem) {
     return siblings;
 };
 
-// dropdown
-let dropdownEl = document.querySelectorAll('.dropdown-option');
-let dropdownClass = document.querySelectorAll('.ctm-dropdown');
-
-let dropdownBtn = document.querySelectorAll('.ddd');
-// let elementID = this.closest('.ctm-dropdown').id;
-
-//     if (dropdownID.classList.contains('show')){
-//     }else {
-//     }
-
-document.addEventListener("click", function(event) {
-    // If user clicks inside the element, do nothing
-    if (event.target.closest(".ctm-dropdown")) return;
-
-    // If user clicks outside the element, hide it!
-    for (let h = 0; h < dropdownClass.length; h++) {
-        dropdownClass[h].classList.remove('show');
-    }
-
-});
-
-for (let g = 0; g < dropdownBtn.length; g++) {
-    dropdownBtn[g].addEventListener("click", function () {
-        let btnID = this.id;
-        this.closest('.ctm-dropdown').classList.toggle('show');
-        let menuAtrr = document.querySelectorAll('.ctm-dropdown__menu');
-        for (let h = 0; h < menuAtrr.length; h++) {
-            let cc = menuAtrr[h].getAttribute('aria-labelledby');
-            if (cc !== btnID) {
-                menuAtrr[h].closest('.ctm-dropdown').classList.remove('show');
-            }
-        }
-
-        // let ff = document.querySelectorAll('.ctm-dropdown__menu');
-        // for (let h = 0; h < ff.length; h++) {
-        //     ff[h].classList.toggle('show');
-        // }
-
-
-        //
-        // let menuLabel = document.querySelectorAll('.ctm-dropdown__menu');
-        // for (let q = 0; q < menuLabel.length; q++) {
-        //     if(btnID === menuLabel[q]) {
-        //     }else {
-        //         // let ctmDropdownSib = getSiblings(this.closest('.ctm-dropdown'));
-        //         console.log('mmmmm')
-        //     }
-        //
-        // }
-
-
-
-
-        // dropdownID.classList.add('show');
-
-        // document.querySelectorAll('.ctm-dropdown').classList.remove('show')
-        // for (let h = 0; h < dropdownClass.length; h++) {
-        //     dropdownClass[h].classList.remove('show');
-        // }
-    });
-}
-
-for (let i = 0; i < dropdownEl.length; i++) {
-    dropdownEl[i].addEventListener('click', function () {
-        let dropdownValue = this.innerHTML;
-        this.closest('.ctm-dropdown').setAttribute('dropdown-value', dropdownValue);
-        this.closest('.ctm-dropdown').classList.remove('show');
-    });
-}
+//
