1e1937a2b5b878c3f1743088833afc17377200a4
Abdulrahman Abotair <ab.hamdy@diva-lab.com>
2020-03-19 22:41:35 +0200

    internals + index

--- js/mainScript.js
+++ js/mainScript.js
@@ -1,6 +1,7 @@
 // Diva-Lab @2020 Main Script
 ////////////////////////////////////////
 
+
 // CSS vh(viewport height) reset
 let jvh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--jvh', `${jvh}px`);
@@ -22,22 +23,132 @@ let getSiblings = function (elem) {
 };
 
 (function () {
-    let z = document.querySelectorAll('.grid .grid__column');
-    for (let n = 0; n < z.length; n++) {
-        if (z.length === 2 ) {
-            z[n].classList.add('grid__column--ending6');
-        } else if(z.length === 1) {
-            z[n].classList.add('grid__column--ending12');
-        }
+    let metroGrd = document.querySelectorAll('.grid');
+    metroMaker(metroGrd);
 
-        let y = z[n].children;
-        for (let o = 0; o < y.length; o++) {
-            if (y.length === 2 ) {
-                y[o].classList.add('grid__item--span6');
-            } else if(y.length === 3) {
-                y[o].classList.add('grid__item--span4');
+    function metroMaker(grdCol) {
+        // loop .grid
+        for (let t = 0; t < grdCol.length; t++) {
+
+            // loop .grid__column
+            let metroColumns = metroGrd[t].children;
+            for (let n = 0; n < metroColumns.length; n++) {
+                if (metroColumns.length === 2) {
+                    metroColumns[n].classList.add('grid__column--ending6');
+                } else if (metroColumns.length === 1) {
+                    metroColumns[n].classList.add('grid__column--ending12');
+                }
+
+                // loop .grid__item
+                let metroBlocks = metroColumns[n].children;
+                for (let o = 0; o < metroBlocks.length; o++) {
+                    if (metroBlocks.length === 2) {
+                        metroBlocks[o].classList.add('grid__item--span6');
+                    } else if (metroBlocks.length === 3) {
+                        metroBlocks[o].classList.add('grid__item--span4');
+                    }
+                }
             }
         }
-
     }
 })();
+// (function () {
+//     let metroColumns = document.querySelectorAll('.grid .grid__column');
+//     metroMaker(metroColumns);
+//
+//     function metroMaker(grdCol) {
+//         for (let n = 0; n < grdCol.length; n++) {
+//             if (grdCol.length === 2) {
+//                 grdCol[n].classList.add('grid__column--ending6');
+//             } else if (grdCol.length === 1) {
+//                 grdCol[n].classList.add('grid__column--ending12');
+//             }
+//
+//             let metroBlocks = grdCol[n].children;
+//             for (let o = 0; o < metroBlocks.length; o++) {
+//                 if (metroBlocks.length === 2) {
+//                     metroBlocks[o].classList.add('grid__item--span6');
+//                 } else if (metroBlocks.length === 3) {
+//                     metroBlocks[o].classList.add('grid__item--span4');
+//                 }
+//             }
+//         }
+//     }
+// })();
+
+
+// toggle sidebar
+$('.side-toggle').on('click', function () {
+    $(this).closest('aside').toggleClass('opened')
+});
+
+// divide one list to multi columns
+(function ($) {
+    let initialContainer = $('.columns'),
+        columnItems = $('.columns li'),
+        columns = null,
+        column = 1; // account for initial column
+
+    console.log(initialContainer);
+    function setupColumns() {
+        columnItems.detach();
+        while (column++ < initialContainer.data('columns')) {
+            initialContainer.clone().insertBefore(initialContainer);
+            column++;
+        }
+        columns = $('.columns');
+    }
+
+    function updateColumns() {
+        column = 0;
+        columnItems.each(function (idx, el) {
+            if (idx !== 0 && idx >= (columnItems.length / columns.length) + (column * idx)) {
+                column += 1;
+            }
+            $(columns.get(column)).append(el);
+        });
+    }
+
+    $(function () {
+        setupColumns();
+        updateColumns();
+    });
+})(jQuery);
+
+// // divide one list to multi columns
+// (function ($) {
+//     let columnsWrapper = document.querySelectorAll('.columns-wrapper');
+//     for (let s = 0; s < columnsWrapper.length; s++) {
+//         let initialContainer = columnsWrapper[s].children;
+//         for (let o = 0; o < initialContainer.length; o++) {
+//             let columnItems =  initialContainer[o].children,
+//                 columns = null,
+//                 column = 1; // account for initial column
+//
+//             function setupColumns() {
+//                 columnItems.detach();
+//                 while (column++ < initialContainer.data('columns')) {
+//                     initialContainer.clone().insertBefore(initialContainer);
+//                     column++;
+//                 }
+//                 columns = $('.columns');
+//             }
+//
+//             function updateColumns() {
+//                 column = 0;
+//                 columnItems.each(function (idx, el) {
+//                     if (idx !== 0 && idx >= (columnItems.length / columns.length) + (column * idx)) {
+//                         column += 1;
+//                     }
+//                     $(columns.get(column)).append(el);
+//                 });
+//             }
+//
+//         }
+//     }
+//
+//     $(function () {
+//         setupColumns();
+//         updateColumns();
+//     });
+// })(jQuery);
