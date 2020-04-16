264241e6ca01c404c9e0446326f49789b818e4f3
Abdulrahman Abotair <ab.hamdy@diva-lab.com>
2020-04-16 16:35:48 +0200

    header nav ul list edit

--- js/mainScript.js
+++ js/mainScript.js
@@ -75,36 +75,36 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 
 // divide one list to multi columns
-    (function ($) {
-        let initialContainer = $('.columns'),
-            columnItems = $('.columns li'),
-            columns = null,
-            column = 1; // account for initial column
-
-        function setupColumns() {
-            columnItems.detach();
-            while (column++ < initialContainer.data('columns')) {
-                initialContainer.clone().insertBefore(initialContainer);
-                column++;
-            }
-            columns = $('.columns');
-        }
-
-        function updateColumns() {
-            column = 0;
-            columnItems.each(function (idx, el) {
-                if (idx !== 0 && idx >= (columnItems.length / columns.length) + (column * idx)) {
-                    column += 1;
-                }
-                $(columns.get(column)).append(el);
-            });
-        }
-
-        $(function () {
-            setupColumns();
-            updateColumns();
-        });
-    })(jQuery);
+//     (function ($) {
+//         let initialContainer = $('.columns'),
+//             columnItems = $('.columns li'),
+//             columns = null,
+//             column = 1; // account for initial column
+//
+//         function setupColumns() {
+//             columnItems.detach();
+//             while (column++ < initialContainer.data('columns')) {
+//                 initialContainer.clone().insertBefore(initialContainer);
+//                 column++;
+//             }
+//             columns = $('.columns');
+//         }
+//
+//         function updateColumns() {
+//             column = 0;
+//             columnItems.each(function (idx, el) {
+//                 if (idx !== 0 && idx >= (columnItems.length / columns.length) + (column * idx)) {
+//                     column += 1;
+//                 }
+//                 $(columns.get(column)).append(el);
+//             });
+//         }
+//
+//         $(function () {
+//             setupColumns();
+//             updateColumns();
+//         });
+//     })(jQuery);
 
     /* *****************************************************************
                             sticky nav script
